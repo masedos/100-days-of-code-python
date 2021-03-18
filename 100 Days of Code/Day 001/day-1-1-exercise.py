9e585d882f3c4231e4f05abfb13f6f8a4cd223f2
Fernandes Macedo <masedos@gmail.com>
2021-03-18 16:54:32 +0000

    day-1-1-exercise

---
+++ 100 Days of Code/Day 001/day-1-1-exercise.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
