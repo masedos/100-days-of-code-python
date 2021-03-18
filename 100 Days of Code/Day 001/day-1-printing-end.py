b426fe02cf469330fa0119adebaf9e5be6fe4b7a
Fernandes Macedo <masedos@gmail.com>
2021-03-18 17:29:03 +0000

    day-1-printing-end.py

--- 100 Days of Code/Day 001/day-1-printing-end.py
+++ 100 Days of Code/Day 001/day-1-printing-end.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Fernandes Macedo'
 __email__ = 'masedos@gmail.com'
 
@@ -8,3 +8,7 @@ print("Hello world!\nHello world!\nHello world!\n")
 
 print("Hello" + " Fernandes Macedo")
 print("Hello" + " " + "Fernandes Macedo")
+
+# print("Bye!")
+print('\n')
+print("Hello " + input("What is your name? ") + "!")
