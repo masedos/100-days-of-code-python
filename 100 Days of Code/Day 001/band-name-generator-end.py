4a4cf0f2f829010ff298026f4c76e818c7134740
Fernandes Macedo <masedos@gmail.com>
2021-03-18 14:32:03 +0000

    #Add generator

---
+++ 100 Days of Code/Day 001/band-name-generator-end.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+__all__ = ['band name generator']
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+print("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up in? \n" )
+pet = input("What's your pt's name? \n" )
+print(f"Your band name could be {city}  {pet}" )
