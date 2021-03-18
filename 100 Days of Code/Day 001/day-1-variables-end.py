63d7a786ee80f1e678de3500cc613ea0104c2b1c
Fernandes Macedo <masedos@gmail.com>
2021-03-18 18:02:30 +0000

    day-1-variables-end.py

---
+++ 100 Days of Code/Day 001/day-1-variables-end.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+name = "Fernandes"
+print(name)
+
+name = "Macedo"
+print(name)
+
+
+name = input("What is your name?")
+length = len(name)
+print(length)
