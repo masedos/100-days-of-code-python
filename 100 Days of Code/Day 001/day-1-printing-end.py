61cc306d4d8aefd1161b30d143792d62c41bdd3f
Fernandes Macedo <masedos@gmail.com>
2021-03-18 17:12:35 +0000

    day-1-2-exercise.py

---
+++ 100 Days of Code/Day 001/day-1-printing-end.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+print("Hello world!\nHello world!\nHello world!\n")
+
+print("Hello" + " Fernandes Macedo")
+print("Hello" + " " + "Fernandes Macedo")
