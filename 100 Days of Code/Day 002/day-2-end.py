90abda4948548e55f6e08495c9fc3dd5dbde1e44
Fernandes Macedo <masedos@gmail.com>
2021-03-19 12:05:44 +0000

    day-2-end.py

---
+++ 100 Days of Code/Day 002/day-2-end.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+num_char = len(input("What is your name? "))
+print("Your name has " + str(num_char) + " characters.")
