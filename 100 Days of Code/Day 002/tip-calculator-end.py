57d39c0f40430ab2e4c0a63d743eee2511df4184
Fernandes Macedo <masedos@gmail.com>
2021-03-19 19:36:20 +0000

    tip-calculator-end.py

--- 100 Days of Code/Day 002/tip-calculator-end.py
+++ 100 Days of Code/Day 002/tip-calculator-end.py
@@ -11,4 +11,4 @@ tip = float(input('What the percentage tip would you like to give? 10, 12, or 15
 
 each = ((bill  * tip / 100) + bill) / people
 
-print(f"Each person should pay: ${each:.1f}")
+print(f"Each person should pay: ${round(each, 2)}")
