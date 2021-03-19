7b6250a9fc8ae5dd3e27638d08473c44630f7d77
Fernandes Macedo <masedos@gmail.com>
2021-03-19 12:12:17 +0000

    day-2-1-exercise.py

---
+++ 100 Days of Code/Day 002/day-2-1-exercise.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+digits = input("Type a two digits number: ")
+print(int(digits[0]) + int(digits[1]))
